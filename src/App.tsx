9f985b82587303054395e51116b6a09197ebc3e7
Tiger Abrodi <tigerabrodi@gmail.com>
2023-12-22 15:45:05 +0100

    setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,8 @@
 export function App() {
-  return <div>hello world</div>;
+  return (
+    <main className="h-full w-full flex-col items-center bg-slate-900">
+      <h1 className="text-5xl font-bold text-slate-400">Hello, world!</h1>
+      <div className="bg-purple-300" />
+    </main>
+  );
 }
