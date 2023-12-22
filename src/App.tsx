7ed5851aad33c47da18ba078e3cf545d57910e36
Tiger Abrodi <tigerabrodi@gmail.com>
2023-12-22 17:02:45 +0100

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,11 @@ export function App() {
     if (typescriptEditorRef.current) {
       tsEditor = monaco.editor.create(typescriptEditorRef.current, {
         language: "typescript",
+        theme: "vs-dark",
+        fontSize: 18,
+        minimap: {
+          enabled: true,
+        },
       });
     }
 
@@ -19,6 +24,11 @@ export function App() {
       jsDisplay = monaco.editor.create(javascriptDisplayRef.current, {
         language: "javascript",
         readOnly: true,
+        theme: "vs-dark",
+        fontSize: 18,
+        minimap: {
+          enabled: true,
+        },
       });
     }
 
@@ -28,17 +38,24 @@ export function App() {
     };
   }, []);
 
+  console.log({
+    typescriptEditorRef: typescriptEditorRef.current,
+  });
+
   return (
-    <main
-      className="h-full w-full flex flex-col items-center p-10 bg-slate-900"
-      style={{ rowGap: 60 }}
-    >
-      <h1 className="text-5xl font-bold text-slate-200">
+    <main className="h-full w-full flex flex-col items-center p-12 bg-slate-400">
+      <h1 className="text-5xl font-bold text-slate-900">
         TypeScript Playground
       </h1>
-      <div className="flex flex-row items-center justify-center gap-4 w-full h-full">
-        <div id="typescript-editor" className="w-3/5 h-5/6" />
-        <div id="javascript-display" className="w-2/6 h-5/6" />
+      <div className="flex flex-row items-center justify-center gap-10 w-11/12 h-4/5 my-auto border-4 border-black">
+        <div
+          ref={typescriptEditorRef}
+          className="w-3/5 h-5/6 shadow-md shadow-black"
+        />
+        <div
+          ref={javascriptDisplayRef}
+          className="w-2/6 h-5/6 shadow-md shadow-black"
+        />
       </div>
     </main>
   );
